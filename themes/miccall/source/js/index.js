ec141b3d11c7e1b49f00d6a706c04ca4ffc9eb2e
Sulthan Nizarudin <n.sultan001@gmail.com>
2020-07-17 17:53:58 +0530

    :)

---
+++ themes/miccall/source/js/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+var app = document.getElementById('app');
+var typewriter = new Typewriter(app, {
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 2,
+});
+
+typewriter.typeString('I am also a ')
+    .typeString('<strong><u>Designer!</u></strong>')
+    .pauseFor(700)
+    .deleteChars(9)
+    .typeString('<strong><u>Programmer!</u></strong>')
+    .pauseFor(700)
+    .deleteChars(11)
+    .typeString('<strong><u>Youtuber!</u></strong>')
+    .pauseFor(700)
+    .deleteChars(9)
+    .typeString('<strong><u>Gamer!</u></strong>')
+    .pauseFor(700)
+    .start();
+});

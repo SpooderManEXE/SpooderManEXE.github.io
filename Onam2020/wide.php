cadc7246969c30b11b0f592b45ad2cd51f5cbd7b
Sulthan Nizarudin <n.sultan001@gmail.com>
2020-08-10 23:04:44 +0530

    Onam

---
+++ Onam2020/wide.php
@@ -0,0 +1,221 @@
+<html>
+<script src="canvas2image.js"></script>
+<link rel="stylesheet" href="bootstrap.min.css">
+<meta name="viewport" content="width=device-width" />
+
+<style>
+@font-face {
+  font-family: LEMONCAKE;
+  src:     url('Lemon-Cake.ttf.woff') format('woff'),
+    url('https://webfonts.ffonts.net/webfonts/L/E/Lemon-Cake/Lemon-Cake.ttf.svg#Lemon-Cake') format('svg'),
+    url('https://webfonts.ffonts.net/webfonts/L/E/Lemon-Cake/Lemon-Cake.ttf.eot'),
+    url('https://webfonts.ffonts.net/webfonts/L/E/Lemon-Cake/Lemon-Cake.ttf.eot?#iefix') format('embedded-opentype');
+    font-weight: normal;
+    font-style: normal;
+}
+body {
+  background: #222;
+  color: #fff;
+  position:absolute;
+  width: 100vw;
+  text-align: center;
+  font-size: 1rem;
+  font-family: sans-serif;
+  padding-bottom: 3em;
+}
+.page-wrap {
+  display: inline-block;
+  margin: 2em auto;
+}
+.controls {
+  &__input {
+    display: block;
+    margin: 0 auto;
+    background: none;
+    border: none;
+    font-size: 1em;
+    padding-bottom: .5em;
+    border-bottom: 2px solid #ccc;
+    text-align: center;
+    outline: none;
+    color: #fff;
+  }
+  &__btn {
+    background: dodgerblue;
+    color: #fff;
+    border: none;
+    font-size: 1em;
+  }
+  &__label {
+     display: block;
+    font-size: .8em;
+    padding-top: .3em;
+    margin-bottom: 2em;
+  }
+}
+
+canvas {
+  background-color: #eee;
+  // opacity: 0;
+  transition: opacity .3s;
+  &.show {
+    opacity: 1;
+  }
+}
+.canvas-wrap {
+  margin-top: 50px;
+  position: relative;
+  width: 100vw;
+}
+#canvasID {
+  z-index: 9999;
+}
+
+</style>
+
+
+<div class="page-wrap">
+  <div class="controls">
+    <style>
+    .mob{
+        margin-top: 10%;
+        font-size: 20px;
+      }
+    @media screen and (min-width: 768px){
+      .mob{
+        margin-top: unset;
+        font-size: unset;
+      }
+    }
+  </style>
+<button type="button" id="download" class="btn btn-primary btn-lg btn-block mob">DOWNLOAD</button>
+<h5><small id="passwordHelpBlock" class="form-text text-muted mb-4">
+REFRESH if text has not loaded.    </small></h5>
+
+
+  </div>
+  <div id="canvas-wrap">
+     <canvas style="display:block;margin-bottom:20px;width:100%;" id="imageCanvas" width=400px height=400px>
+        <canvas id="canvasID"></canvas>
+    </canvas>
+  </div>
+
+</div>
+<script>
+var nam="<?php echo $_GET["name"] ?>";
+var wish="<?php echo $_GET["wish"] ?> ";
+
+</script>
+<script>
+
+var text_title ="";
+var imageLoader = document.getElementById('imageLoader');
+    window.addEventListener('load', handleImage, false);
+var canvas = document.getElementById('imageCanvas');
+var ctx = canvas.getContext('2d');
+var img = new Image();
+img.crossOrigin="anonymous";
+
+window.addEventListener('load', DrawPlaceholder)
+
+function DrawPlaceholder() {
+    img.onload = function() {
+        DrawOverlay(img);
+        DrawText();
+        DynamicText(img)
+    };
+    //img.src = 'https://unsplash.it/400/400/?random';
+
+}
+function DrawOverlay(img) {
+    ctx.drawImage(img,0,0);
+    ctx.moveTo(150, 20);
+    ctx.lineTo(150, 170);
+    ctx.fillStyle = 'rgba(30, 144, 255, 0.0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+function DrawText() {
+    ctx.fillStyle = "#ff0302";
+    ctx.textBaseline = 'middle';
+    ctx.font = "150px 'LEMONCAKE'";
+    ctx.textAlign = "start";
+    ctx.fillText("HAPPY ONAM!", 1100, 400);
+    ctx.fillStyle = "#00c2a0";
+    ctx.font = "100px 'LEMONCAKE'";
+    var x=1100;
+    var y=520;
+    var words = wish.split(' ');
+    var line = '';
+    var firstY=y;
+    var lineHeight=75*1.286; // a good approx for 10-18px sizes
+
+    for(var n = 0; n < words.length; n++) {
+      if(y<910){
+    var testLine = line + words[n] + ' ';
+    var metrics = ctx.measureText(testLine);
+    var testWidth = metrics.width;
+    if(testWidth > 1190) {
+      ctx.fillText(line.toUpperCase(), x, y);
+      if(n<words.length-1){
+          line = words[n] + ' ';
+          y += lineHeight;
+      }
+    }
+    else {
+      line = testLine;
+    }
+  }}
+  ctx.fillText(line.toUpperCase(), x, y);
+  var w=document.getElementById('wname');
+  ctx.textAlign = "end";
+  ctx.fillStyle = "#1f506e";
+  ctx.fillText("~ "+nam.toUpperCase(), x+1200, 1030);
+
+    //ctx.fillText(text_title, 1100, 500);
+}
+function DynamicText(img) {
+  //document.getElementById('but').addEventListener('click', function() {
+    setTimeout(function() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    DrawOverlay(img);
+    DrawText();
+    text_title = this.value;
+    wtext_title = w.value;
+    //ctx.fillText(text_title, 1100, 500);
+  }, 25);
+}
+function handleImage(e) {
+  if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+    var reader = new FileReader();
+    var img = new Image();
+        img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src = "wimg.jpg";
+        var src = "wimg.jpg";
+        canvas.classList.add("show");
+        DrawOverlay(img);
+        DrawText();
+        DynamicText(img);
+
+
+    reader.readAsDataURL(e.target.files[0]);
+
+}
+function convertToImage() {
+	window.open(canvas.toDataURL('png'));
+}
+document.getElementById('download').onclick = function download() {
+		//convertToImage();
+    Canvas2Image.saveAsPNG(canvas, 1920, 1080, "Wish_Wide")
+}
+
+
+</script>
+</html>
---
+++ onam.php
@@ -0,0 +1,85 @@
+<html>
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" href="Onam2020/bootstrap.min.css">
+<style>
+.mob{
+    margin:25% 0 10% 0;
+  }
+  body {
+   background-image: url("Onam2020/bgs.jpg");
+   background-color: white;
+   background-size: contain;
+  }
+
+@media screen and (min-width: 768px){
+  .mob{
+    margin:8% 25%;
+    -webkit-box-flex:0;
+    -ms-flex:0 0 50%;
+    flex:0 0 50%;
+    max-width:50%;
+    align-self:center!important;
+  }
+  body {
+   background-image: url("Onam2020/bgw.jpg");
+   background-color: white;
+   background-size: cover;
+  }
+}
+</style>
+<body style="overflow: hidden">
+
+<script>
+function setLocation(element) {
+        document.getElementById('PForm').action = element.value;
+}
+
+</script>
+
+<div class="mob">
+<form class="text-center border border-light p-5" id="PForm" action="#" method="get">
+
+    <p class="h4 mb-4">ONAM CELEBRATION POSTER GENERATOR</p>
+
+
+    <p class="h4 mb-2">NAME</p><input type="text" id="defaultLoginFormEmail" class="form-control mb-4" name="name">
+
+
+    <p class="h4 mb-2">CUSTOM WISH</p><textarea type="text" name="wish" id="wish" class="form-control mb-2" maxlength=125 rows=4><?php
+$a=array("Here’s wishing you health, wealth and happiness. May the spirit of Onam remain everywhere. Cheers to the beautiful festival.","Let the sweet frangrance of flowers fill our homes and the aroma of home-cooked delicacies tease our senses.","On this joyous occasion of Onam, I wish you joy and good health and may you always enjoy the bounty of nature!", "May King Mahabali bless you with good health and happiness. May all your hopes, dreams and wishes come true.", "May the colours and joy of Onam fill your home and heart with happiness and prosperity. May you always enjoy a good fortune.");
+shuffle($a);
+$random_keys=array_rand($a,5);
+echo $a[$random_keys[0]];
+?></textarea>
+    <h5><small id="passwordHelpBlock" class="form-text text-muted mb-4">
+'Happy Onam' is already included    </small></h5>
+    <div class="d-flex justify-content-around">
+      <div class="form-check">
+<input class="form-check-input" type="radio" name="t" id="exampleRadios1" value="Onam2020/wide.php" onclick="setLocation(this)">
+<label class="form-check-label" for="exampleRadios1">
+  Wide Poster
+</label>
+</div>
+<div class="form-check">
+<input class="form-check-input" type="radio" name="t" id="exampleRadios2" value="Onam2020/story.php" onclick="setLocation(this)">
+<label class="form-check-label" for="exampleRadios2">
+  Long Story Poster
+</label>
+</div>
+    </div>
+
+    <button class="btn btn-info btn-block" type="submit">GENERATE POSTER</button>
+
+</form>
+</div>
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center">Subscribe:
+    <a href="https://www.youtube.com/channel/UCmszvVQKqM7v5PR_nMrm7YQ/"> CraftCoder by Sulthan Nizarudin</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+</body>
+</html>
